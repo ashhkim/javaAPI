3522688eb63b0335ffc62ded00655e8e6defcc9d
Kareem Shehab <kshehab1@outlook.com>
2019-10-04 17:17:55 -0400

    adjusted meters

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -12,26 +12,29 @@ export class ScoreComponent implements OnInit {
     public employee_score = [];
     
     gaugeType = "full";
-    UWGaugeVal = 1/2;
+    UWGaugeVal = 0;
     UWLabel = "UW Point";
 
-    FICOGaugeVal = 500;
+    FICOGaugeVal = 800;
     FICOLabel = "FICO";
 
     size= window.innerWidth / 7.5;
 
     thickness = 10;
 
-    max = 850;
-    min = 300;
+    FICOmax = 850;
+    FICOmin = 300;
 
+    UWmax = 10;
+    UWmin = 0;
+    
     cap="round";
 
     thresholdConfig = {
-        '0': {color: 'conic-gradient(red, blue)'},
-        '630': {color: 'red'},
-        '690': {color: '#2ecc71'},
-        '720': {color: '#27ae60'}
+        '0': {color: '#c0392b'},
+        '630': {color: '#e67e22'},
+        '690': {color: '#f1c40f'},
+        '720': {color: '#2ecc71'}
     };
 
     constructor(private _employeeService: EmployeeService){ }
@@ -40,6 +43,11 @@ export class ScoreComponent implements OnInit {
         this._employeeService.getScore().subscribe(data => { 
             this.employee_score = data; 
             // this.FICOGaugeVal = this.employee_score[0].ficoScore;
+            this.UWGaugeVal = this.employee_score[0].uwPointNumer;
         });
+    }
+
+    resizeGauge() {
+        this.size = window.innerWidth / 7.5;
     }
 }
